8009cdc4ae31c52c4b4326355e2b77fa12b6d423
micakenobi <beckyedithbrooker@gmail.com>
2017-03-10 20:46:13 -0800

    Here is Question 3

--- class-05-images-color-text/lab/app.js
+++ class-05-images-color-text/lab/app.js
@@ -51,11 +51,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
+  var d = sum(a,b)[0];
+  var e = sum(d,c)[0];
+  var f = multiply(a,b)[0];
+  var g = multiply(f,c)[0];
+  var returnSumString = a + ' and ' + b + ' and ' + c + ' sum to ' + e + '.';
+  var returnMultiplyString = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + g + '.';
 
+  return [e, returnSumString, returnMultiplyString, g];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
