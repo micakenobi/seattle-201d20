8017e5ee8f01b22a78261021b55e53f88a621119
micakenobi <beckyedithbrooker@gmail.com>
2017-03-10 22:12:13 -0800

    Here is problem 4. Enjoy, it took forever.

--- class-05-images-color-text/lab/app.js
+++ class-05-images-color-text/lab/app.js
@@ -80,12 +80,17 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2,3,4]; //eslint-disable-line
 
 function sumArray(testArray){ //eslint-disable-line
-
+  var runningSum = 0;
+  for (var i = 0; i < testArray.length; i++){
+    runningSum = sum(runningSum, testArray[i])[0];
+  }
+  var elementString = testArray + ' was passed in as an array of numbers, and ' + runningSum + ' is their sum.';
+  return [runningSum, elementString];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
